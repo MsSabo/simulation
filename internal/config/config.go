e5e22e2bc61b84a71f7b0aac76290c678782325e
Sabyrbek <sakuba61@gmail.com>
2025-04-10 23:46:59 +0600

    Add config parsing xml

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,46 @@
 package config
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+// список обьектов симуляции
+const (
+	Grass string = "grass"
+	Tree  string = "tree"
+	Rock  string = "rock"
+	Sheep string = "sheep"
+	Fox   string = "fox"
+)
 
 type Config struct {
-	XmlName xml.Name `xml:"simulation:config"`
-	Amount  []struct {
-		Grass int `xml:"Grass"`
-		Tree  int `xml:"Tree"`
-		Sheep int `xml:"Sheep"`
-		Fox   int `xml:"Fox"`
-		Rock  int `xml:"Rock"`
-	} `xml:"amount"`
+	XmlName xml.Name      `xml:"simulation"`
+	Entity  []EntityParam `xml:"entity"`
+}
+
+type EntityParam struct {
+	Name     string `xml:"name"`
+	Quantity int    `xml:"quantity"`
+}
+
+func ParseConfig(file string) (*Config, error) {
+	cfg := new(Config)
+	data, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	err = xml.Unmarshal(data, cfg)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	fmt.Printf("--- Unmarshal ---\n\n")
+	fmt.Printf("%+v\n", cfg)
+
+	return cfg, nil
 }
